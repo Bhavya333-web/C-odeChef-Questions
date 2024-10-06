23dc4003c56f08230dd9507028ba211a8bd426aa
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-06 20:19:43 +0530

    c++

---
+++ Cricket_world_cup_qualifier.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+if (X >= 12) {
+    cout << "Yes" << endl;
+} else {
+    cout << "No" << endl;
+}
+}
