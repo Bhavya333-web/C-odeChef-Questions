18b50ba66b2aef1d2b0c85e8b1786064b49e099f
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-07 14:13:33 +0530

    c++

---
+++ Masterchef_finals.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin>>T;
+for(int i = 0 ; i<T ; i++){
+    int X;
+    cin>>X;
+    if(X<=10){
+        cout<<"YES"<<endl;
+        
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    
+    
+}
+}
