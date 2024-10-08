2a1d5bc5b90c84c12ce09bd584bdf46b8dd1b286
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-08 17:10:09 +0530

    c++

---
+++ chef_score_check.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin>>T;
+for(int i=0;i<T;i++){
+    int X,N;
+    cin>>X>>N;
+    cout<<(X/10)*N<<endl;
+    
+}
+}
