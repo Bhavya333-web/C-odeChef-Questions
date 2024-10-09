06748976fba30ef5f1983d04bd6d75fe8dc2ac1e
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-09 16:16:55 +0530

    c++

--- lucky_seven.cpp
+++ lucky_seven.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    // Declare a string variable
+    
     string S;
     
-    // Input the string of length 10
+    
     cin >> S;
     
     // Output the 7th character (index 6)
