26e6b5478d55da632e410760bc571d48bdbb47bd
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-09 16:06:59 +0530

    c++

---
+++ ticket_rush.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin>>T;
+for(int i =0 ; i<T;i++){
+    int X,Y;
+    cin>>X>>Y;
+    if(Y>=X){
+        cout<<"0"<<endl;
+    }
+    else if(Y<X){
+        cout<<X-Y<<endl;
+    }
+}
+}
