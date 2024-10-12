4606819da0084600fb49e16e00252b9c70467893
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-12 23:10:56 +0530

    c++

---
+++ Best_of_two.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++){
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(X>Y){
+	        cout<<X<<endl;
+	    }
+	    else if(X<Y){
+	        cout<<Y<<endl;
+	        
+	    }
+	    else if(X==Y){
+	        cout<<X<<endl;
+	    }
+	    else{
+	        cout<<"Default";
+	    }
+	}
+
+}
