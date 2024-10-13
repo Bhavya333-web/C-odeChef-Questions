2964d3f8ca8ea7603154d973a8f78dc4a8b97e02
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-13 17:33:05 +0530

    c++

--- Best_of_two.cpp
+++ Best_of_two.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 	int T;
 	cin>>T;
 	for(int i=0;i<T;i++){
 	    int X,Y;
 	    cin>>X>>Y;
+		
 	    if(X>Y){
 	        cout<<X<<endl;
 	    }
