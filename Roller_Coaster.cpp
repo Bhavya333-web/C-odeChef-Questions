26082d0e21e171c9a4513f33b7ef4def1a932468
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-13 17:20:06 +0530

    c++

---
+++ Roller_Coaster.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin>>T;
+for(int i=0;i<T;i++){
+    int X,H;
+    cin>>X>>H;
+    if(X>=H){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+}
