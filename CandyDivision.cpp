dbad92cdd5ed70f2a9b0857f0a73eb7ba8564a7f
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-13 17:32:34 +0530

    c++

--- CandyDivision.cpp
+++ CandyDivision.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	
+    
 int T;
 cin>>T;
 for(int i=0;i<T;i++){
--- Roller_Coaster.cpp
+++ Roller_Coaster.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 int T;
 cin>>T;
 for(int i=0;i<T;i++){
-    int X,H;
-    cin>>X>>H;
-    if(X>=H){
+    int X,Y;
+    cin>>X>>Y;
+    if(X>=Y){
         cout<<"YES"<<endl;
     }
     else{
