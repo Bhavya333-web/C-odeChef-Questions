b834b2d26fd4a4135838cd97cc9e21837641a25c
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-13 17:35:04 +0530

    c++

--- Alice_Happiness.cpp
+++ Alice_Happiness.cpp
@@ -1,11 +1,6 @@
-/*Alice Happiness Condition
-Alice has scored 
-X
-X marks in her test and Bob has scored 
-Y
-Y marks in the same test. Alice is happy if she scored at least twice the marks of Bob’s score. Print "Yes" if she is happy.*/
 #include <iostream>
 using namespace std;
+
 int main(){
 int X,Y,Z;
 cin>>X>>Y;
