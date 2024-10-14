7dc8fae2443b55c5cd18da81f749bb83858349e5
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-14 18:47:40 +0530

    c++

--- lucky_seven.cpp
+++ lucky_seven.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main() {
     
-    string S;
-    
-    
-    cin >> S;
-    
+string S;  
+    cin >> S; 
     // Output the 8th character(index 7)
-    
     cout << S[7] << endl;
     
     return 0;
+    
 }
+
