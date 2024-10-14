87f6f30536014a18642f8c2f2f5f1e3df2164a83
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-14 18:45:34 +0530

    c++

--- Double_value.cpp
+++ Double_value.cpp
@@ -3,25 +3,21 @@ using namespace std;
 
 // Function to double the value of an integer using a pointer
 void doubleValue(int* p) {
+    
     // Complete this function
 }
-
 int main() {
     // Define an integer variable and take input
     int x;
     
     cin >> x;
    
-
-    // Create a pointer to the integer variable
     int* ptr = &x;
-
     
     // Call the function to double the value of the integer using the pointer
     doubleValue(ptr);
 
     // Print the final value of the integer
     cout << 2*x << endl;
-
     return 0;
 }
