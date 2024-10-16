ebbec459f81ac6977af3468b13f747f08609c141
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-16 09:10:12 +0530

    c++

---
+++ Four_Friends.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0; i<T;i++){
+	    int X;
+	    cin>>X;
+	    if(4*X<=1000){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
