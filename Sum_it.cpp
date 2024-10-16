2fc0d807d44d121a02ffccdbae75b76c0503fe71
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-16 09:19:08 +0530

    c++

---
+++ Sum_it.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	for(int i=0; i<T;i++){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(A+B==C){
+	    cout<<"yes"<<endl;
+	    }
+	    else {
+	        cout<<"NO"<<endl;
+	    }
+	   
+	
+
+}
+
+}
