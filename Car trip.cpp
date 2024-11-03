2932f6a121b6cf2cdb70aca2f7c019d990f4183f
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-11-03 21:31:22 +0530

    Solution - CodeChef

---
+++ Car trip.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T;
+cin >>T;
+for(int i =0;i<T;i++){
+    int X;
+    cin >>X;
+    if(X<300){
+        cout<<300*10<<endl;
+    }
+    else if(X>=300){
+        cout<<X*10<<endl;
+    }
+}
+}
